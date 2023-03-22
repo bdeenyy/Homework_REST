e204b56dff745a0ef4122aa9b6ef363cdc7088b7
bdeenyy <109365710+bdeenyy@users.noreply.github.com>
2023-03-22 17:34:40 +0300

    Initial commit

---
+++ src/main/java/com/example/homework_rest/HomeworkRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_rest/HomeworkRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
